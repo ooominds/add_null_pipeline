1c7050fd90b7d8c1043eb6ad7ea3f183fe7440f7
tukkvwa <t.kwakpovwe@gmail.com>
2022-05-11 15:12:34 +0100

    sequence sampling

--- add_null.py
+++ add_null.py
@@ -2,8 +2,10 @@
 from numpy import array, savetxt, transpose
 from nltk import RegexpParser, tree
 from os import system
+from os.path import exists
 from sys import argv
 import logging
+from subprocess import check_output
 import argparse
 
 logging.basicConfig(level=logging.INFO)
@@ -38,6 +40,7 @@ def add_null(infile="200_extra_sentences.txt", outfile="nulled_sentences",  nphr
             #the line is a string in the form of tuples that represent a word and tag pair
             if '\\' in line:
                 line = line.replace('\\', '#')
+            line = "[{}]".format(line)
             eval_line = eval(line)
             #print(eval_line)
             #to store a sentence
@@ -89,13 +92,13 @@ def extract_all_sentences(cur_file, new_file):
         if sen_count == 200:
             with open("{}.txt".format(new_file), 'a') as nf:
                 for sentence in sen_list:
-                    nf.write("[{}] \n".format(sentence))
+                    nf.write("{}\n".format(sentence))
                 sen_count = 0
                 sen_list = []
                 sentence = ""
     with open("{}.txt".format(new_file), 'a') as nf:
         for sentence in sen_list:
-            nf.write("{} \n".format(sentence))
+            nf.write("{}".format(sentence))
 
 # Maybe make a tagger later
 def add_tags(infile):
@@ -112,9 +115,13 @@ def main():
     parser.add_argument('in_file', type=str, help='location of the POS-tagged corpus .txt file stored as sentence per line, each line contains sequences of tuples that form a sentence, the tuples contain a word and POS-tag')
     parser.add_argument('out_file', type=str, help='location of the output file. A .txt file with two columns, one for a word and the other for the POS-tag (with null article tags)')
     
-    parser.add_argument('-sa', type=bool, default=True, help='A random sample from the in_file a specific number of sentences')
+    parser.add_argument('-sa', action='store_true', help='Sample random sentences from the in_file')
+    parser.add_argument('-sr', action='store_true', help='Sample random sentences from the in_file as well as r sentence before and l sentences after')
     parser.add_argument('-se', type=int, default=500, help='a random seed')
     parser.add_argument('-n', type=int, default=500, help='number of sentences to sample (requires sample flag to be set)')
+    parser.add_argument('-c', type=int, default=-1, help='number of sentences before and after the sampled sentence to include')
+    parser.add_argument('-b', type=int, default=3, help='number of sentences before a sampled sentence to include')
+    parser.add_argument('-a', type=int, default=3, help='number of sentences after a sampled sentence to include')
 
     args = parser.parse_args()
 
@@ -122,15 +129,33 @@ def main():
     logger.info("Location of POS-tagged file as nltk-compatible sentences (per line): {}".format(args.in_file))
     logger.info("POS-tagged file with null-articles: {}".format(args.out_file))
     
-    extract_all_sentences(args.og_file, args.in_file)
+    if not exists("{}.txt".format(args.in_file)):
+        extract_all_sentences(args.og_file, args.in_file)
+    else:
+        logger.info("{} already exists, skipping sentence file creation step".format(args.in_file))
     # Next use the following command in the Linux terminal: shuf -n 500 [infile].txt > [infile]_[n].txt
     if args.sa:
         logger.info("Number of smapled sentences: {}".format(args.n))
-        system("shuf -n {} {}.txt > {}_{}.txt".format(args.n, args.in_file, args.in_file, args.n))
-        args.in_file = "{}_{}".format(args.in_file, args.n)
+        system("shuf -n {} {}.txt > {}_sa_{}.txt".format(args.n, args.in_file, args.in_file, args.n))
+        args.in_file = "{}_sa_{}".format(args.in_file, args.n)
+
+    elif args.sr:
+        if args.c == -1:
+            logger.info("Number of smapled sentences: {} \n sentences before: {} \n sentence after: {}".format(args.n, args.b, args.a))
+            system("shuf -n {} {}.txt | xargs -I % grep -F -B {} -A {} % {}.txt > {}_sr_{}.txt".format(args.n, args.in_file, args.b, args.a, args.in_file, args.in_file, args.n))
+            args.in_file = "{}_sr_{}".format(args.in_file, args.n)
+        else:
+            logger.info("Number of smapled sentences: {} \n sentences before: {} \n sentence after: {}".format(args.n, args.c, args.c))
+            #p = check_output("shuf -n {} {}.txt | xargs -I % grep -C {} % {}.txt".format(args.n, args.in_file, args.c, args.in_file), shell=True)
+            #print(p)
+            system("shuf -n {} {}.txt | xargs -I % grep -F -C {} % {}.txt > {}_sr_{}.txt".format(args.n, args.in_file, args.c, args.in_file, args.in_file, args.n))
+            args.in_file = "{}_sr_{}".format(args.in_file, args.n)
+
     add_null(args.in_file, args.out_file)
 
 if __name__ == "__main__":
+    #shuf stagged_c5_500.txt -n 5 | grep -B 6 stagged_c5_500.txt
+    #shuf stagged_c5_500.txt -n 2 | xargs -I % grep -B 1 -A 1 % stagged_c5_500.txt
     #infile, outfile = sys.argv[1:3]
     #main(infile, outfile)
     main()
